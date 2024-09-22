2d5dfd8e1581c296588a5df7154f2ccde447d688
bipinlongjam <bipinlongjam11@gmail.com>
2024-09-23 01:20:50 +0530

    Sequelize

--- util/database.js
+++ util/database.js
@@ -1,10 +1,22 @@
-const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-    host:'localhost',
-    user:'root',
-    database:'node-complete',
-    password:'Bipin@0810'
-})
 
-module.exports = pool.promise();
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('node-complete', 'root', 'Bipin@0810', {
+  dialect: 'mysql',
+  host: 'localhost'
+});
+
+module.exports = sequelize;
+
+
+// const mysql = require('mysql2');
+
+// const pool = mysql.createPool({
+//     host:'localhost',
+//     user:'root',
+//     database:'node-complete',
+//     password:'Bipin@0810'
+// })
+
+// module.exports = pool.promise();
